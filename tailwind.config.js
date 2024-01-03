2f2383df8501dccfc94bf8ce9d07698cc9aa4b2b
Daniel A <DanielAAad01@gmail.com>
2024-01-02 21:40:33 -0800

    Custom assets added to tailwind.config.js, basic structure and styling completed for app container excluding input fields.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,25 @@
 /** @type {import('tailwindcss').Config} */ 
 module.exports = {
-    content: ["./src/**/*.{html,js}"],
+    content: ["./src/**/*.{html,js}", "./*.{html,js}"],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                primary: {
+                    purple: "hsl(259, 100%, 65%)",
+                    lightRed: "hsl(0, 100%, 67%)"
+                },
+                neutral: {
+                    white: "hsl(0, 0%, 100%)",
+                    offWhite: "hsl(0, 0%, 94%)",
+                    lightGrey: "hsl(0, 0%, 86%)",
+                    smokeyGrey: "hsl(0, 1%, 44%)",
+                    offBlack: "hsl(0, 0%, 8%)",
+                }
+            },
+            fontFamily: {
+                'poppins': ['Poppins']
+            }
+        },
     },
     plugins: [],
 }
